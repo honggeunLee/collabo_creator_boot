cbfaf02949c960b6b769a349dcdc95161eec6bc2
Honggeun Lee <kwy05115@gmail.com>
2024-11-26 14:43:03 +0900

    Initial commit to AHN

---
+++ src/main/java/org/example/collabo_creator_boot/CollaboCreatorBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.collabo_creator_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaboCreatorBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollaboCreatorBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/collabo_creator_boot/CollaboCreatorBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.collabo_creator_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaboCreatorBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
