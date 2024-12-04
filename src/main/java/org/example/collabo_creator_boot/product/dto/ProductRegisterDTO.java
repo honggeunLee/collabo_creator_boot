5fadf8c10e9bc8d9cda972bdb5dd8cc08545c8b5
Honggeun Lee <kwy05115@gmail.com>
2024-12-04 11:02:51 +0900

    S3 사진 업로드 기능 추가

--- src/main/java/org/example/collabo_creator_boot/product/dto/ProductRegisterDTO.java
+++ src/main/java/org/example/collabo_creator_boot/product/dto/ProductRegisterDTO.java
@@ -1,11 +1,9 @@
 package org.example.collabo_creator_boot.product.dto;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 import java.util.List;
 
