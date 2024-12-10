3760d1067af98f4d8443d5687a7f9c3f576019e2
Honggeun Lee <kwy05115@gmail.com>
2024-12-10 13:11:25 +0900

    action test

--- src/main/java/org/example/collabo_creator_boot/config/WebConfig.java
+++ src/main/java/org/example/collabo_creator_boot/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로 허용
-                .allowedOrigins("http://localhost:5173") // 허용할 Origin
+                .allowedOrigins("http://43.201.252.29:80") // 허용할 Origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보 포함 허용
